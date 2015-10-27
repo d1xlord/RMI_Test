7c779e6e610943dd9ca3db133e54d67bc1758d52
Nafis Ahmed <nafis@tigerit.com>
2015-10-27 15:30:11 +0600

    Changes made in RMIClient to use only RmiServer

--- src/RMIClient.java
+++ src/RMIClient.java
@@ -8,13 +8,13 @@ public class RMIClient {
     public static void  main(String args[]) throws Exception{
         String host = "192.168.104.64:1099";
         TestService test = (TestService) Naming.lookup("//" + host + "/RmiServer");
-        TestService2 test2 = (TestService2) Naming.lookup("//" + host + "/RmiServer2");
+//        TestService2 test2 = (TestService2) Naming.lookup("//" + host + "/RmiServer2");
         System.out.println("Enter a number : ");
         Scanner scan = new Scanner(System.in);
         int val = scan.nextInt();
         System.out.println("Square of this is : " + test.getSquare(val));
 
-        String str = scan.nextLine();
-        System.out.println(test2.getString("Nafis"));
+//        String str = scan.nextLine();
+//        System.out.println(test2.getString("Nafis"));
     }
 }
